ae8a52f12f7b304d1c58fcc04be5db3060286fb1
Purnendu Bhowmik Shuvro <bhowmikpurnendu1@gmail.com>
2025-06-29 21:15:46 +0600

    Add files via upload

---
+++ Lab_Task/Lab_02/JS Files/app.js
@@ -0,0 +1,126 @@
+// alert (" Hello from external file !") ;
+
+/* const name = "John";
+console.log(name); */ 
+
+/* const person = {
+name: "Alice",
+age: 30
+};
+console.log (person); // Output : { name : " Alice " , age : 30 }
+
+const numbers = [1 , 2 , 3 , 4];
+console.log (numbers) ; // Output : [1 , 2 , 3 , 4] */
+
+/*let message = "Hello , world !";
+const PI = 3.1415;
+var oldVariable = 42;
+console.log(message); */
+
+/* let data = 10; // number
+data = "ten "; // now a string
+console.log(data); */
+
+/* let a = 5;
+let b = 2;
+console .log (a + b) ; // 7
+console .log (a % b) ; // 1 ( modulus ) */
+
+//console.log("Hello World");
+
+/* let x = 10;
+x += 5; // x = 15
+console.log(x); */
+
+/* console.log (5 == "5") ; // true ( loose equality )
+console.log (5 === "5") ; // false ( strict equality ) */
+
+//let b = 2; 
+/* if ( b%2 == 0 ) {
+    console.log("even");
+ } else {
+    console.log("odd");
+ } */
+
+
+/* let day;
+switch (new Date().getDay()) {
+  case 0:
+    day = "Sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 2:
+    day = "Tuesday";
+    break;
+  case 3:
+    day = "Wednesday";
+    break;
+  case 4:
+    day = "Thursday";
+    break;
+  case 5:
+    day = "Friday";
+    break;
+  case  6:
+    day = "Saturday";
+}
+console.log("Today is " + day); */
+
+/* for(let i=0; i <5; i++) {
+console.log (i) ;
+ } */
+
+/* let i = 0;
+while (i < 5) {
+console.log (i) ;
+i++;
+} */
+
+/* let i = 0;
+do {
+console.log (i) ;
+i++;
+} while (i < 5) ; */
+
+/* function greet ( name ) {
+return " Hello " + name ;
+}
+
+console .log ( greet (" Alice ") ) ; */
+
+/* const greet = function ( name ) {
+return "Hi " + name ;
+ };
+
+console .log ( greet ("Bob ") ) ; */
+
+/* const greet = name => "Hey " + name ;
+
+console .log ( greet (" Charlie ") ) ;*/
+
+/* let a = 5;
+let b = 1;
+function multiply (a, b) {
+return a * b;
+ }
+ console.log(multiply(a,b)); */
+
+/* const person = {
+name : " Alice ",
+age : 25 ,
+greet : function () {
+console .log ("Hello , " + this . name ) ;
+}
+}; 
+
+person . greet () ; // Hello , Alice */
+
+let colors = ["red", " green ", " blue "];
+
+//console .log ( colors [0]) ; // "red "
+colors . push (" yellow ") ; // add item
+//colors . pop () ; // remove last
+
+colors . forEach ( color => console .log ( color ) ) ;
